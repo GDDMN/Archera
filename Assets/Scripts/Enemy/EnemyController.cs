75a7fb8b54927e3c12589147bd0b9607d2ca9a5f
GDDMN <apawkin@gmail.com>
2024-04-28 14:35:48 +0300

    update melee enemy logic

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public abstract class EnemyController : MonoBehaviour
 {
-    
+  protected NavMeshAgent _navAgent;
+
+  [SerializeField] protected Animator _animator;
+
 }
---
+++ Assets/Scripts/Enemy/MeleeEnemyController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MeleeEnemyController : EnemyController
+{
+  [SerializeField] private PlayerMoveController _player;
+
+  private void Awake()
+  {
+    _navAgent = GetComponent<NavMeshAgent>();
+  }
+
+  private void Update()
+  {
+    FindWayToPlayer();
+    _animator.SetFloat("Speed", _navAgent.speed);
+  }
+
+  private void FindWayToPlayer()
+  {
+    if (_player == null)
+      return;
+
+    _navAgent.SetDestination(_player.Transform.position);
+  }
+}
